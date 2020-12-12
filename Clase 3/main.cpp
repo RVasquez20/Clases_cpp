1e1b8c7e95d6a4d278ef090a2a61697106d0d2c9
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2020-12-12 00:08:02 -0600

    ejercicios

--- Clase 3/main.cpp
+++ Clase 3/main.cpp
@@ -16,7 +16,12 @@ cout<<"1) sumar"<<endl;
 cout<<"2) Restar"<<endl;
 cout<<"3) Multiplicar"<<endl;
 cout<<"4) Dividir"<<endl;
-cout<<"5) Salir"<<endl;
+cout<<"5) Modulo"<<endl;
+cout<<"6) suma del 1-1000"<<endl;
+cout<<"7) Numeros pares del 1-360"<<endl;
+cout<<"8) Promedio de n notas"<<endl;
+cout<<"9) Numeros impares del 1-360"<<endl;
+cout<<"10) Salir"<<endl;
 cout<<"Ingrese la opcion que desea: ";
 cin>>opc;
 
@@ -30,12 +35,58 @@ cin>>opc;
             break;
         }
 
-        
-
         case 5:{
             cout<<"Gracias por utilizar el programa"<<endl;
             getch();
             exit(0);
+            break;
+        }
+
+        case 6:{
+            int suma=0;
+            for(int i=1;i<=1000;i++){
+                cout<<"No."<<i<<endl;
+                suma+=i;
+            }
+            cout<<"La suma es "<<suma<<endl;
+            break;
+        }
+        case 7:{
+            for(int i=1;i<=360;i++){
+                if(i%2==0){
+                    cout<<i<<endl;
+                }
+            }
+            break;
+        }
+        case 8:{
+            int nota=0,suma=0,contador=0;
+            double promedio=0;
+            string opc;
+            do {
+             cout<<"Ingrese la nota:";
+             cin>>nota;
+             suma+=nota;
+             contador++;
+             fflush(stdin);
+             cout<<"Desea ingresar otra nota? S/N"<<endl;
+             getline(cin,opc);
+            }while(opc=="s"||opc=="S");
+            promedio=suma/contador;
+            if(promedio>=61){
+                cout<<"El promedio es :" <<promedio<<" esta Aprovado"<<endl;
+            }else{
+                cout<<"El promedio es :" <<promedio<<" esta Reprobado"<<endl;
+            }
+
+            break;
+        }
+        case 9:{
+            for(int i=1;i<=360;i++){
+                if(i%2==1){
+                    cout<<i<<endl;
+                }
+            }
             break;
         }
         default:{
