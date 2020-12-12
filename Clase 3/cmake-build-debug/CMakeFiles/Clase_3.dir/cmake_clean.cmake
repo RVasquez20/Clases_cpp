adffc2716230c266b68873859cb8f9e38330d731
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2020-12-11 23:34:01 -0600

    clase 3

---
+++ Clase 3/cmake-build-debug/CMakeFiles/Clase_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clase_3.dir/main.cpp.obj"
+  "Clase_3.exe"
+  "Clase_3.exe.manifest"
+  "Clase_3.pdb"
+  "libClase_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clase_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
