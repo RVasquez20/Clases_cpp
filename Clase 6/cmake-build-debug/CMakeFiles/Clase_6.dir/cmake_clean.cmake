915f1ccff2767e7bb45affbcbf6f300a668c81fc
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2020-12-22 00:09:35 -0600

    Clase 6

---
+++ Clase 6/cmake-build-debug/CMakeFiles/Clase_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clase_6.dir/main.cpp.obj"
+  "Clase_6.exe"
+  "Clase_6.exe.manifest"
+  "Clase_6.pdb"
+  "libClase_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clase_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
