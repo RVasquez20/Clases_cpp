20458e1a44cf382033b9ae9288ef4d4e982a0ba6
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-02-16 00:08:06 -0600

    Clase 23
    Tarea y ficheros

---
+++ Clase 23/cmake-build-debug/CMakeFiles/Clase_23.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Clase_23.dir/main.cpp.obj"
+  "Clase_23.exe"
+  "Clase_23.exe.manifest"
+  "Clase_23.pdb"
+  "libClase_23.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Clase_23.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
