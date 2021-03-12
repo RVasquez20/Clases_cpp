c4a9db7a8e9ec1afbf84e9ec83fef829c180d45d
Snowden <55126792+Snowdenv20@users.noreply.github.com>
2021-03-12 01:04:44 -0600

    clase 31
    namespace, documentacion,cabeceras y librerias

---
+++ Clase 31/Calcu.h
@@ -0,0 +1,17 @@
+//
+// Created by Rodrigo on 12/03/2021.
+//
+#include "calculadora.h"
+float suma(float n1,float n2){
+    return n1+n2;
+}
+float resta(float n1,float n2){
+    return n1-n2;
+}
+float multiplicacion(float n1,float n2){
+    return n1*n2;
+}
+float division(float n1,float n2){
+    return n1/n2;
+}
+
---
+++ Clase 31/calculadora.h
@@ -0,0 +1,17 @@
+//
+// Created by Rodrigo on 12/03/2021.
+//
+
+#ifndef CLASE_31_CALCULADORA_H
+#define CLASE_31_CALCULADORA_H
+float suma(float n1,float n2);
+float resta(float n1,float n2);
+/**
+ * esto funciona para multiplicar toma dos datos que son:
+ * @param n1 numero1,
+ * @param n2 numero2,
+ * @return y nos devuelve numero1*numero2
+ */
+float multiplicacion(float n1,float n2);
+float division(float n1,float n2);
+#endif //CLASE_31_CALCULADORA_H
